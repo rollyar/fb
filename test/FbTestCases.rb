f1af8849e2dcb81280990d81c7e36dc91071ff51
Rolando Daniel Arnaudo <rolando.arnaudo@gmail.com>
2026-02-19 15:22:54 -0300

    Fix test setup for CI: use tmp directory and env vars
    - Use Dir.tmpdir for Linux platform instead of /var/fbdata
    - Use FIREBIRD_HOST, FIREBIRD_USER, FIREBIRD_PASSWORD env vars
    - Clean up .fdb extension files

--- test/FbTestCases.rb
+++ test/FbTestCases.rb
@@ -1,13 +1,12 @@
 require 'fileutils'
 include FileUtils
+
 require 'fb'
 
-if RUBY_VERSION =~ /^2/
+if RUBY_VERSION.match?(/^2/)
   require 'minitest/autorun'
 
-  unless Minitest.const_defined?('Test')
-    Minitest::Test = MiniTest::Unit::TestCase
-  end
+  Minitest::Test = MiniTest::Unit::TestCase unless Minitest.const_defined?('Test')
 
   class FbTestCase < Minitest::Test
   end
@@ -16,8 +15,7 @@ else
   require 'test/unit'
 
   class FbTestCase < Test::Unit::TestCase
-    def default_test
-    end
+    def default_test; end
   end
 end
 
@@ -26,22 +24,25 @@ module FbTestCases
 
   def setup
     @db_file = case RUBY_PLATFORM
-      when /win32/ then 'c:/var/fbdata/drivertest.fdb'
-      when /darwin/ then File.join(File.expand_path(File.dirname(__FILE__)), 'drivertest.fdb')
-      else '/var/fbdata/drivertest.fdb'
-    end
-    @db_host = 'localhost'
-    @username = 'sysdba'
-    @password = 'masterkey'
+               when /win32/ then 'c:/var/fbdata/drivertest.fdb'
+               when /darwin/ then File.join(__dir__, 'drivertest.fdb')
+               when /linux/ then File.join(Dir.tmpdir, 'drivertest.fdb')
+               else '/var/fbdata/drivertest.fdb'
+               end
+    @db_host = ENV['FIREBIRD_HOST'] || 'localhost'
+    @username = ENV['FIREBIRD_USER'] || 'sysdba'
+    @password = ENV['FIREBIRD_PASSWORD'] || 'masterkey'
     @parms = {
-      :database => "#{@db_host}:#{@db_file}",
-      :username => @username,
-      :password => @password,
-      :charset => 'NONE',
-      :role => 'READER' }
+      database: "#{@db_host}:#{@db_file}",
+      username: @username,
+      password: @password,
+      charset: 'NONE',
+      role: 'READER'
+    }
     @parms_s = "database = #{@db_host}:#{@db_file}; username = #{@username}; password = #{@password}; charset = NONE; role = READER;"
     @fb_version = -1
     rm_rf @db_file
+    rm_rf "#{@db_file}.fdb"
 
     Database.create(@parms) do |connection|
       d = connection.query("SELECT substring(rdb$get_context('SYSTEM', 'ENGINE_VERSION') from 1 for 1) from rdb$database")
@@ -66,9 +67,9 @@ end
 
 class Fb::Connection
   def execute_script(sql_schema)
-    self.transaction do
+    transaction do
       sql_schema.strip.split(';').each do |stmt|
-        self.execute(stmt);
+        execute(stmt)
       end
     end
   end

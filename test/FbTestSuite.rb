5ab80d1a8eca67267bf2fca73b4ffb0470730b7c
Rolando Daniel Arnaudo <rolando.arnaudo@gmail.com>
2026-02-20 08:01:36 -0300

    Enable RETURNING tests

--- test/FbTestSuite.rb
+++ test/FbTestSuite.rb
@@ -7,6 +7,6 @@ require 'CursorTestCases'
 require 'DataTypesTestCases'
 require 'NumericDataTypesTestCases'
 require 'TransactionTestCases'
-# require 'ReturningTestCases'  # Disabled - RETURNING feature under development
+require 'ReturningTestCases' # RETURNING feature
 require 'EncodingTestCases' if RUBY_VERSION.match?(/^1.9/)
 require 'bigdecimal'
